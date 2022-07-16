53a35bc1b887a0977831f634685dc722160ab74b
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2022-07-16 19:59:39 +0200

    bumped  to release 1.0.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='aws-cloudformation-power-switch',
-    version="1.0.4",
+    version="1.0.5",
     url='https://github.com/binxio/aws-cloudformation-power-switch',
     license='BSD',
     author='Mark van Holsteijn',
